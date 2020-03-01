dcdeab44350ff19eb6605de37109166ae8d61436
CsabaVK <vaganykarc@outlook.com>
2020-03-01 21:25:47 +0100

    upload

---
+++ Application/Templates/newCarView.php
@@ -0,0 +1,16 @@
+<h2>Autó hozzáadása</h2>
+<table style="width:100%">
+
+<form method="POST" action="<?=ROOT?>?newCar">
+  <tr>
+    <th>Producer <input type="text" name="producer"></th>
+    <th>Type <input type="text" name="type"></th>
+    <th>Vintage <input type="text" name="vintage"></th>
+    <th>Fuel <input type="text" name="fuel"></th>
+    <th><button type="submit">Add</button></th>
+  </tr>
+</form>
+
+
+
+</table>
